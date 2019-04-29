20623c07b57ceff47a01ecef5be6963df80ebb76
Regiivania <50116900+Regiivania@users.noreply.github.com>
2019-04-29 15:22:15 -0300

    Add files via upload

---
+++ shooter3V2019/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1556561551,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/damage-sheet0.png",
+		"images/arrowupbutton-sheet0.png",
+		"images/arrowupbutton-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
